c995042ed2891fb2e7681f67eaece728a6cc34f3
Raul Hernandez Alonso <rhernandez@openrh.es>
2015-07-25 19:39:39 +0200

    se rehace el enlace a preguntas y se adapta para usar la BBDD

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,17 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', 					   quizController.index);
+router.get('/quizes/:quizId(\\d+)', 	   quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author', function(req, res){
 	res.render('author');
 });
+
+/*
 router.get('/vistaBuscar', function(req, res){
 	res.render('quizes/vistaBuscar');
 });
+*/
+
 module.exports = router;
