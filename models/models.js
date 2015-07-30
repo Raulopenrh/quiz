b0c3f91bf595271639f43f66e524aa7a17cda1a7
Raul Hernandez Alonso <rhernandez@openrh.es>
2015-07-30 19:12:17 +0200

    Se corrige un bug en models.js que hacia fallar heroku

--- models/models.js
+++ models/models.js
@@ -28,8 +28,8 @@ var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz;
-sequelize.sync().success(function(){
-	Quiz.count().success(function(count){
+sequelize.sync().then(function(){
+	Quiz.count().then(function(count){
 		if(count === 0){
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'			  
